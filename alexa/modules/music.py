03f31de25ffba48707959926b5d9ea2dfaa65da0
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-20 00:44:22 +0530

    add exception for http error

--- alexa/modules/music.py
+++ alexa/modules/music.py
@@ -730,20 +730,23 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
-    subprocess.run(
-        'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done',
-        shell=True)
-    l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.reply("sending the song")
-    await event.client.send_file(
+    try:
+     subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
+     subprocess.run(
+         'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done',
+         shell=True)
+     l = glob.glob("*.mp3")
+     loa = l[0]
+     await event.reply("sending the song")
+     await event.client.send_file(
         event.chat_id,
         loa,
         force_document=False,
         allow_cache=False,
         supports_streaming=True,
         caption=cmd,
-        reply_to=reply_to_id,
-    )
-    subprocess.run("rm -rf *.mp3", shell=True)
+        reply_to=reply_to_id)
+     os.system("rm -rf *.mp3")
+    except Exception:
+      await event.reply("I am getting too many requests !\nPlease try again later.")
+    
