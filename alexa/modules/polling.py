1452db3d5c1853d9d18689813ee60d6c111ae8a1
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-20 14:18:05 +0530

    Update polling.py

--- alexa/modules/polling.py
+++ alexa/modules/polling.py
@@ -2,11 +2,52 @@
 
 from telethon import *
 from telethon.tl import *
-from alexa import register
+from alexa.events import register
+from pymongo import MongoClient
+from alexa import MONGO_DB_URI
+from alexa.events import register
+from alexa import tbot
+from telethon.tl import functions, types
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client['test']
+approved_users = db.approve
+
+
+#------ THANKS TO LONAMI ------#
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
+
 
 @register(pattern="^/poll (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*)")
 async def _(event):
-   
+    approved_userss = approved_users.find({})
+    for ch in approved_userss: 
+        iid = ch['id']
+        userss = ch['user']
+    if event.is_group:
+     if (await is_register_admin(event.input_chat, event.message.sender_id)):
+       pass
+     elif event.chat_id == iid and event.from_id == userss:  
+       pass
+     else:
+       return
     quiz = event.pattern_match.group(1)
     if "True" in quiz:
        quizy = True
