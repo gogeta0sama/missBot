87659f656f2ce2ceedbaba968fc3b26bf7118e0a
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-21 00:30:53 +0530

    Update misc.py

--- alexa/modules/misc.py
+++ alexa/modules/misc.py
@@ -4362,6 +4362,62 @@ async def aexec(code, smessatatus):
 
 
 
+@alexabot(pattern=".eval")
+async def _(event):
+    check = event.message.sender_id
+    checkint = int(check)
+    # print(checkint)
+    if int(check) != int(OWNER_ID):
+        return
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success 😃"
+
+    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await ubot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+            
+    else:
+        await event.edit(final_output)
+
+
 __help__ = """
  - /id: get the current group id. If replied to user's message gets that user's id.
  - /runs: reply a random string from an array of replies.
