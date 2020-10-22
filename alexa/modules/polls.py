5daf89690b2b7a244f39e29d7e2d449fb9b105b0
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-22 10:47:00 +0530

    Update polls.py

--- alexa/modules/polls.py
+++ alexa/modules/polls.py
@@ -6,15 +6,19 @@ You can now send polls anonymously with Alexa
 Here is how you can do it:
 
 *Parameters* -
+ ▪️ poll_id - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
  ▪️ question - the question you wanna ask
  ▪️ <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
  ▪️ <True/False>(2) - public votes 
  ▪️ <True/False>(3) - multiple choice 
 
 *Syntax* - 
-`/poll <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
+`/poll <poll_id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
 
 *Examples* -
-`/poll am i cool? | False False False yes no`
-`/poll am i cool? | True@1 False False yes no`
+`/poll 12345 | am i cool? | False False False yes no`
+`/poll 12345 | am i cool? | True@1 False False yes no`
+
+*To stop a poll*
+Reply to the poll with /stoppoll <poll_id> to stop the poll
 """
