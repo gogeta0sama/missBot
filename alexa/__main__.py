5ab04812f33a61d15e2db7521bf63f47af85d97e
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-23 21:42:56 +0530

    Update __main__.py

--- alexa/__main__.py
+++ alexa/__main__.py
@@ -1173,4 +1173,5 @@ def main():
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: ", str(ALL_MODULES))
     tbot.start(bot_token=TOKEN)
+    LOGGER.info("Successfully started Alexa !")
     main()
