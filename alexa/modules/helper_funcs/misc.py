8d08a648c8ab88e2e02a43d84c988a1cddae06a2
MissAlexaRobot <54566209+MissAlexaRobot@users.noreply.github.com>
2020-10-23 17:55:51 +0530

    Update misc.py

--- alexa/modules/helper_funcs/misc.py
+++ alexa/modules/helper_funcs/misc.py
@@ -731,12 +731,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix,
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    # can only have a certain amount of buttons side by side
-    #    if len(pairs) > 7:
-    #        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-    #            (EqInlineKeyboardButton("<<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-    # EqInlineKeyboardButton(">>>", callback_data="{}_next({})".format(prefix,
-    # modulo_page)))]
+    pairs += [[EqInlineKeyboardButton("Home ▶️", callback_data="bot_start")]]
 
     return pairs
 
