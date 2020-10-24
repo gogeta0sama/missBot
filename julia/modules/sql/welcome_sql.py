73311d6882cbb7af9a9bfd5c79d294b225532dec
MissJuliaRobot <54566209+MissJuliaRobot@users.noreply.github.com>
2020-10-24 18:01:28 +0530

    Update welcome_sql.py

--- julia/modules/sql/welcome_sql.py
+++ julia/modules/sql/welcome_sql.py
@@ -674,7 +674,7 @@ from julia.modules.sql import BASE
 from julia.modules.sql import SESSION
 
 DEFAULT_WELCOME = "Hi {first}, how are you?"
-DEFAULT_GOODBYE = "{first} has left the chat."
+DEFAULT_GOODBYE = "Nice knowing you!"
 
 
 class Welcome(BASE):
