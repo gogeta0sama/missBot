598dfd63d6657597a72142a07f20d6bf33bb27a1
MissAlexaRobot <ayushchatterjee2003@gmail.com>
2020-10-24 16:15:20 +0530

    rebase to julia

---
+++ julia/modules/afk.py
@@ -0,0 +1,150 @@
+from julia import dispatcher
+from julia.modules.sql import afk_sql as sql
+from julia.modules.users import get_user_id
+from telegram import MessageEntity, Update
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, Filters, MessageHandler, run_async, CommandHandler
+import time
+from telegram import ParseMode
+
+AFK_GROUP = 7
+AFK_REPLY_GROUP = 8
+
+
+@run_async
+def afk(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split(None, 1)
+    user = update.effective_user
+
+    if not user:  # ignore channels
+        return
+
+    if user.id == 777000:
+        return
+    fname = update.effective_user.first_name
+    notice = ""
+    if len(args) >= 2:
+        reason = args[1]
+        if len(reason) > 100:
+            reason = reason[:100]
+            notice = "{fname} your afk reason was shortened to 100 characters."
+    else:
+        reason = ""
+    start_time = time.time()
+    sql.set_afk(update.effective_user.id, reason, start_time)
+    fname = update.effective_user.first_name
+    try:
+        update.effective_message.reply_text("*{} is now AFK !*\n\n{}".format(
+            fname, notice), parse_mode=ParseMode.MARKDOWN)
+    except BadRequest:
+        pass
+
+
+@run_async
+def no_longer_afk(update: Update, context: CallbackContext):
+    user = update.effective_user
+    message = update.effective_message
+
+    if not user:  # ignore channels
+        return
+
+    res = sql.rm_afk(user.id)
+    if res:
+        if message.new_chat_members:  # dont say msg
+            return
+        firstname = update.effective_user.first_name
+        try:
+            text = "*{} is no longer AFK !*".format(firstname)
+            update.effective_message.reply_text(
+                text, parse_mode=ParseMode.MARKDOWN)
+        except BaseException:
+            return
+
+
+@run_async
+def reply_afk(update: Update, context: CallbackContext):
+    bot = context.bot
+    message = update.effective_message
+    userc = update.effective_user
+    userc_id = userc.id
+    if message.entities and message.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
+        entities = message.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+
+        chk_users = []
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                user_id = ent.user.id
+                fst_name = ent.user.first_name
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+
+            if ent.type == MessageEntity.MENTION:
+                user_id = get_user_id(message.text[ent.offset:ent.offset +
+                                                   ent.length])
+                if not user_id:
+                    # Should never happen, since for a user to become AFK they
+                    # must have spoken. Maybe changed username?
+                    return
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+
+                try:
+                    chat = bot.get_chat(user_id)
+                except BadRequest:
+                    print("Error: Could not fetch userid {} for AFK module"
+                          .format(user_id))
+                    return
+                fst_name = chat.first_name
+
+            else:
+                return
+
+            check_afk(update, context, user_id, fst_name, userc_id)
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+        check_afk(update, context, user_id, fst_name, userc_id)
+
+
+def check_afk(update, context, user_id, fst_name, userc_id):
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+        if not user.reason:
+            etime = user.start_time
+            elapsed_time = time.time() - float(etime)
+            final = time.strftime("%Hh: %Mm: %Ss", time.gmtime(elapsed_time))
+
+            if int(userc_id) == int(user_id):
+                return
+            res = "*{} is AFK !*\n\n*Last seen*: {}".format(fst_name, final)
+            update.effective_message.reply_text(
+                res, parse_mode=ParseMode.MARKDOWN)
+        else:
+            etime = user.start_time
+            elapsed_time = time.time() - float(etime)
+            final = time.strftime("%Hh: %Mm: %Ss", time.gmtime(elapsed_time))
+
+            if int(userc_id) == int(user_id):
+                return
+            res = "*{} is AFK !*\n\n*Reason*: {}\n\n*Last seen*: {}".format(
+                fst_name, user.reason, final)
+            update.effective_message.reply_text(
+                res, parse_mode=ParseMode.MARKDOWN)
+
+
+AFK_HANDLER = CommandHandler("afk", afk)
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
+
+dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
+
+__mod_name__ = "AFK"
---
+++ julia/modules/polling.py
@@ -0,0 +1,353 @@
+# MADE BY @MissAlexa_Robot
+# He has wasted a lot of time of his life to make this so please give
+# proper credits to him 😭
+
+from telethon import *
+from telethon.tl import *
+from julia.events import register
+from pymongo import MongoClient
+from julia import MONGO_DB_URI
+from julia import tbot
+
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client['test']
+approved_users = db.approve
+dbb = client['poll']
+poll_id = dbb.pollid
+
+#------ THANKS TO LONAMI ------#
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
+# syntax : /poll 12345 | am i cool? | False False False yes no
+# syntax : /poll 12345 | am i cool? | True@1 False False yes no
+
+
+@register(pattern="^/poll (.*)")
+async def _(event):
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch['id']
+        userss = ch['user']
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.from_id == userss:
+            pass
+        else:
+            return
+    try:
+        quew = event.pattern_match.group(1)
+    except Exception:
+        await event.reply("Where is the question ?")
+        return
+    if "|" in quew:
+        secrets, quess, options = quew.split("|")
+    secret = secrets.strip()
+
+    if not secret:
+        await event.reply("I need a poll id of 5 digits to make a poll")
+        return
+
+    try:
+        secret = int(secret)
+    except ValueError:
+        await event.reply("Poll id should contain only numbers")
+        return
+
+    count = 0
+    while(int(secret) > 0):
+        count = count + 1
+        secret = int(secret) // 10
+
+    if count != 5:
+        await event.reply("Poll id should be an integer of 5 digits")
+        return
+
+    allpoll = poll_id.find({})
+    for c in allpoll:
+        if event.from_id == c['user'] and secret == c['pollid']:
+            await event.reply("Please give another poll id, this id is already used")
+            return
+        poll_id.insert_one({'user': event.from_id, 'pollid': secret})
+
+    ques = quess.strip()
+    option = options.strip()
+    quiz = option.split(' ')[1 - 1]
+    if "True" in quiz:
+        quizy = True
+        if "@" in quiz:
+            one, two = quiz.split("@")
+            rightone = two.strip()
+        else:
+            await event.reply("You need to select the right answer with question number like True@1, True@3 etc..")
+            return
+
+        quizoptionss = []
+        try:
+            ab = option.split(' ')[4 - 1]
+            cd = option.split(' ')[5 - 1]
+            quizoptionss.append(types.PollAnswer(ab, b'1'))
+            quizoptionss.append(types.PollAnswer(cd, b'2'))
+        except Exception:
+            await event.reply("At least need two options to create a poll")
+            return
+        try:
+            ef = option.split(' ')[6 - 1]
+            quizoptionss.append(types.PollAnswer(ef, b'3'))
+        except Exception:
+            ef = None
+        try:
+            gh = option.split(' ')[7 - 1]
+            quizoptionss.append(types.PollAnswer(gh, b'4'))
+        except Exception:
+            gh = None
+        try:
+            ij = option.split(' ')[8 - 1]
+            quizoptionss.append(types.PollAnswer(ij, b'5'))
+        except Exception:
+            ij = None
+        try:
+            kl = option.split(' ')[9 - 1]
+            quizoptionss.append(types.PollAnswer(kl, b'6'))
+        except Exception:
+            kl = None
+        try:
+            mn = option.split(' ')[10 - 1]
+            quizoptionss.append(types.PollAnswer(mn, b'7'))
+        except Exception:
+            mn = None
+        try:
+            op = option.split(' ')[11 - 1]
+            quizoptionss.append(types.PollAnswer(op, b'8'))
+        except Exception:
+            op = None
+        try:
+            qr = option.split(' ')[12 - 1]
+            quizoptionss.append(types.PollAnswer(qr, b'9'))
+        except Exception:
+            qr = None
+        try:
+            st = option.split(' ')[13 - 1]
+            quizoptionss.append(types.PollAnswer(st, b'10'))
+        except Exception:
+            st = None
+
+    elif "False" in quiz:
+        quizy = False
+    else:
+        await event.reply("Wrong arguments provided !")
+        return
+
+    pvote = option.split(' ')[2 - 1]
+    if "True" in pvote:
+        pvoty = True
+    elif "False" in pvote:
+        pvoty = False
+    else:
+        await event.reply("Wrong arguments provided !")
+        return
+    mchoice = option.split(' ')[3 - 1]
+    if "True" in mchoice:
+        mchoicee = True
+    elif "False" in mchoice:
+        mchoicee = False
+    else:
+        await event.reply("Wrong arguments provided !")
+        return
+    optionss = []
+    try:
+        ab = option.split(' ')[4 - 1]
+        cd = option.split(' ')[5 - 1]
+        optionss.append(types.PollAnswer(ab, b'1'))
+        optionss.append(types.PollAnswer(cd, b'2'))
+    except Exception:
+        await event.reply("At least need two options to create a poll")
+        return
+    try:
+        ef = option.split(' ')[6 - 1]
+        optionss.append(types.PollAnswer(ef, b'3'))
+    except Exception:
+        ef = None
+    try:
+        gh = option.split(' ')[7 - 1]
+        optionss.append(types.PollAnswer(gh, b'4'))
+    except Exception:
+        gh = None
+    try:
+        ij = option.split(' ')[8 - 1]
+        optionss.append(types.PollAnswer(ij, b'5'))
+    except Exception:
+        ij = None
+    try:
+        kl = option.split(' ')[9 - 1]
+        optionss.append(types.PollAnswer(kl, b'6'))
+    except Exception:
+        kl = None
+    try:
+        mn = option.split(' ')[10 - 1]
+        optionss.append(types.PollAnswer(mn, b'7'))
+    except Exception:
+        mn = None
+    try:
+        op = option.split(' ')[11 - 1]
+        optionss.append(types.PollAnswer(op, b'8'))
+    except Exception:
+        op = None
+    try:
+        qr = option.split(' ')[12 - 1]
+        optionss.append(types.PollAnswer(qr, b'9'))
+    except Exception:
+        qr = None
+    try:
+        st = option.split(' ')[13 - 1]
+        optionss.append(types.PollAnswer(st, b'10'))
+    except Exception:
+        st = None
+
+    if pvoty is False and quizy is False and mchoicee is False:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=optionss,
+                quiz=False)))
+
+    if pvoty is True and quizy is False and mchoicee is True:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=optionss,
+                quiz=False,
+                multiple_choice=True,
+                public_voters=True)))
+
+    if pvoty is False and quizy is False and mchoicee is True:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=optionss,
+                quiz=False,
+                multiple_choice=True,
+                public_voters=False)))
+
+    if pvoty is True and quizy is False and mchoicee is False:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=optionss,
+                quiz=False,
+                multiple_choice=False,
+                public_voters=True)))
+
+    if pvoty is False and quizy is True and mchoicee is False:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=quizoptionss,
+                quiz=True
+            ),
+            correct_answers=[f"{rightone}"]))
+
+    if pvoty is True and quizy is True and mchoicee is False:
+        await tbot.send_file(event.chat_id, types.InputMediaPoll(
+            poll=types.Poll(
+                id=12345,
+                question=ques,
+                answers=quizoptionss,
+                quiz=True,
+                public_voters=True
+            ),
+            correct_answers=[f"{rightone}"]))
+
+    if pvoty is True and quizy is True and mchoicee is True:
+        await event.reply("You can't use multiple voting with quiz mode")
+        return
+    if pvoty is False and quizy is True and mchoicee is True:
+        await event.reply("You can't use multiple voting with quiz mode")
+        return
+
+
+@register(pattern="^/stoppoll (.*)")
+async def stop(event):
+    secret = event.pattern_match.group(1)
+    approved_userss = approved_users.find({})
+    for ch in approved_userss:
+        iid = ch['id']
+        userss = ch['user']
+    if event.is_group:
+        if (await is_register_admin(event.input_chat, event.message.sender_id)):
+            pass
+        elif event.chat_id == iid and event.from_id == userss:
+            pass
+        else:
+            return
+
+    try:
+        secret = int(secret)
+    except ValueError:
+        await event.reply("Poll id should contain only numbers")
+        return
+
+    count = 0
+    while(int(secret) > 0):
+        count = count + 1
+        secret = int(secret) // 10
+
+    if count != 5:
+        await event.reply("Poll id should be an integer of 5 digits")
+        return
+
+    if not event.reply_to_msg_id:
+        await event.reply("Please reply to a poll to stop it")
+        return
+    else:
+        try:
+            msg = await event.get_reply_message()
+            c = await tbot.get_me()
+            if str(msg.from_id) != str(c.id):
+                await event.reply("I can't do this operation on this poll.\nProbably it's not created by me")
+                return
+            if msg.poll:
+                allpoll = poll_id.find({})
+                for c in allpoll:
+                    if not event.from_id == c['user'] and secret == c['pollid']:
+                        await event.reply("Oops, either you haven't created this poll or you have given wrong poll id")
+                        return
+                    poll_id.delete_one(
+                        {'user': event.from_id, 'pollid': secret})
+                    pollid = msg.poll.poll.id
+                    await msg.edit(file=types.InputMediaPoll(
+                        poll=types.Poll(
+                            id=pollid,
+                            question="",
+                            answers=[],
+                            closed=True)))
+                    await event.reply("Successfully stopped the poll")
+            else:
+                await event.reply("This isn't a poll")
+        except Exception:
+            await event.reply("I can't do this operation on this poll.\nProbably it's already closed")
+            return
